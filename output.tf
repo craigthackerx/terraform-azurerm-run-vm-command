135f8367f8e492e0778979890107d6f03fc08094
Craig Thacker <craigthackerx@gmail.com>
2021-02-23 01:16:46 +0000

    fix naming

--- output.tf
+++ output.tf
@@ -1,7 +1,4 @@
 output "vm_id" {
   value = data.azurerm_virtual_machine.azure_vm.id
-}
-
-output "vm_name" {
-  value = data.azurerm_virtual_machine.azure_vm.id
+  description = "The ID of the VM you use in the module."
 }
